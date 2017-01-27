58b6d014c85b212f71b3821215ec317482727f0a
grindlock <sergioenriqueperez@gmail.com>
2017-01-26 21:27:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sergio/calculator_android/Calc_Screen.java
@@ -0,0 +1,13 @@
+package com.example.sergio.calculator_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calc_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_screen);
+    }
+}
